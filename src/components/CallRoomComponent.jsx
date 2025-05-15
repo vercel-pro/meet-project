52a0e2e4eee3de40953574697e7e0829c6fe5bb8
Vercel <vercel@devsramen.com>
2025-05-15 11:47:49 +0600

    Finally Done Meet app at 15-05-2025_1147.

--- src/components/CallRoomComponent.jsx
+++ src/components/CallRoomComponent.jsx
@@ -5,16 +5,27 @@ import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 const CallRoomComponent = () => {
 
     const {roomCode} = useParams()
+
+    // generate Kit Token
     const myMeeting = async(element) => {
-        const appID = "1644128239";
+        const appID = 1644128239;
         const serverSecret = "57fc94e7bc07c8b784de8befd2633ee5";
-        const kitToken = ZegoUIKitPrebuilt
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomCode, Date.now().toString(), "Please enter your name");
+        
+        // Create instance object from Kit Token.
+        const zp = ZegoUIKitPrebuilt.create(kitToken);
+        zp.joinRoom({
+            container: element,
+            scenario: {
+                mode: ZegoUIKitPrebuilt.VideoConference,
+            },
+        })
     }
 
 
     return (
         <>
-            ${roomCode}
+            <div ref={myMeeting} className='w-full h-screen bg-green-100'></div>
         </>
     );
 };
